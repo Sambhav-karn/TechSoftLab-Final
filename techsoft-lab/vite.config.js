14553ea3034b157437a7fe15c4dd4e5ceb0c0f05
Sambhav Kumar <153364087+Sambhav-karn@users.noreply.github.com>
2025-05-06 10:43:14 +0530

    Update vite.config.js

--- techsoft-lab/vite.config.js
+++ techsoft-lab/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  server: {
+    allowedHosts: ['techsoftlab.in'], // Add your allowed host here
+  },
 })
